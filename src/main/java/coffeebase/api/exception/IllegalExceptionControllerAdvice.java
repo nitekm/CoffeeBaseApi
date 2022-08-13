8ecec6f4af7b82702a60001e41a8ca89634858e7
Whinterfresh <maciej.nitek@gmail.com>
2022-08-13 21:24:14 +0200

    CB-11 Verify if exceptions returning proper messages and codes from good reasons

--- src/main/java/coffeebase/api/exception/IllegalExceptionControllerAdvice.java
+++ src/main/java/coffeebase/api/exception/IllegalExceptionControllerAdvice.java
@@ -1,5 +1,7 @@
 package coffeebase.api.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,18 +11,23 @@ import javax.validation.ConstraintViolationException;
 @RestControllerAdvice(annotations = IllegalExceptionProcessing.class)
 public class IllegalExceptionControllerAdvice {
 
+    private Logger logger = LoggerFactory.getLogger(IllegalExceptionControllerAdvice.class);
+
     @ExceptionHandler(IllegalArgumentException.class)
     ResponseEntity<?> handleIllegalArgument(IllegalArgumentException e) {
+        logger.error(e.getMessage());
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 
     @ExceptionHandler(IllegalStateException.class)
     ResponseEntity<String> handleIllegalState(IllegalStateException e) {
+        logger.error(e.getMessage());
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     ResponseEntity<String> handleConstraintViolation(ConstraintViolationException e) {
+        logger.error(e.getMessage());
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
