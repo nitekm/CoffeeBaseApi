9ad8b61697721c7216f06fd30b630e3b8b43cf4c
ncode <developerncode@gmail.com>
2024-01-13 21:07:51 +0100

    CB-161 Secret Manager

--- src/main/java/coffeebase/api/config/MvcConfig.java
+++ src/main/java/coffeebase/api/config/MvcConfig.java
@@ -1,20 +1,15 @@
 package coffeebase.api.config;
 
 import coffeebase.api.aspect.interceptor.LoggerInterceptor;
-import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
         registry.addInterceptor(new LoggerInterceptor());
-    }
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
     }
 }
